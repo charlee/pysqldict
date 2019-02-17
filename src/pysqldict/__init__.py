ea4aa550f53af2a672bafe61eac711b1f10bbd67
Charlee Li <oda.charlee@gmail.com>
2019-02-16 19:36:01 -0500

    add document

--- src/pysqldict/__init__.py
+++ src/pysqldict/__init__.py
@@ -4,4 +4,10 @@ __version__ = '0.1.0'
 
 
 def open(filename=':memory:'):
+    """Opens a SQLite database file. Returns a database object that can be used to
+    retrieve tables.
+
+    :param filename: Path of the database file. Use memory database (`:memory:`) if omitted.
+    :rtype: Database object.
+    """
     return SqlDict(filename)
